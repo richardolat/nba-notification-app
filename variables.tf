452c1fd277db8879234d3a216b1da57045fe3f81
richardolat <134428528+richardolat@users.noreply.github.com>
2025-01-31 15:11:55 -0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+# SES Sender Email (Verified SES email)
+variable "ses_email" {
+  description = "Verified SES sender email address"
+  type        = string
+  sensitive   = true
+}
+
+# SES Recipient Email (Verified SES email)
+variable "recipient_email" {
+  description = "Verified SES recipient email address"
+  type        = string
+  sensitive   = true
+}
+
+# API Key for sportsdata.io to fetch NBA scores
+variable "sports_api_key" {
+  description = "API key for accessing sportsdata.io to fetch NBA game data"
+  type        = string
+  sensitive   = true
+}
